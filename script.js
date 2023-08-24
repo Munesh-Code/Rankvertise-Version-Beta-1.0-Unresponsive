6462824b84be7bd2eb313dcb5ab28148637e106f
Munesh Kumar Singh <81091614+Munesh-Code@users.noreply.github.com>
2023-08-24 15:52:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ window.onload = function (){
 
 
 
-let crsr = document.querySelector("#cursor");
-let blur = document.querySelector("#cursor-blur");
-document.addEventListener("mousemove",function (dets){
-  crsr.style.left = dets.x + "px";
-  crsr.style.top = dets.y + "px";
-  blur.style.left = dets.x - 50 + "px";
-  blur.style.top = dets.y - 50 + "px";
-});
+// let crsr = document.querySelector("#cursor");
+// let blur = document.querySelector("#cursor-blur");
+// document.addEventListener("mousemove",function (dets){
+//   crsr.style.left = dets.x + "px";
+//   crsr.style.top = dets.y + "px";
+//   blur.style.left = dets.x - 50 + "px";
+//   blur.style.top = dets.y - 50 + "px";
+// });
 
 
 
